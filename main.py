fa6430c20b95bc1dd0a86b251f8e4cff04ab5763
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-02-02 13:05:34 +0100

    Gyakorlas

---
+++ main.py
@@ -0,0 +1,10 @@
+def fo_program(karakter1, karakter2):
+
+    if karakter1 > 1:
+        print("Laza vagyok")
+    elif karakter2 > karakter1:
+        print("Kemény vagyok")
+
+
+fo_program(karakter1=int(input("Kérek egy számot: ")), karakter2=3)
+
